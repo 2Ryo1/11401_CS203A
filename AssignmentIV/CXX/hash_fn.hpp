b644b698c5ee6c04613ac95a72db7b18ad170e4e
Ryo Ito <ryo0516tw@gmail.com>
2025-11-30 20:49:12 +0900

    Update hash_fn.hpp

--- AssignmentIV/CXX/hash_fn.hpp
+++ AssignmentIV/CXX/hash_fn.hpp
@@ -9,7 +9,7 @@
    Development History:
     - 2025/11/17: Initial implementation
 
-   Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
+   Developer: Ryo Ito <s1133352@mail.yzu.edu.tw>
  */
 #ifndef HASH_FN_HPP
 #define HASH_FN_HPP
@@ -20,3 +20,4 @@ int myHashString(const std::string& str, int m);
 int myHashInt(int key, int m);
 
 #endif
+
